a4d00a147274088d616d59be69e6a627e0698237
robert604 <robtemper@gmail.com>
2022-01-09 11:49:16 -0500

    test commit after changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,42 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+import { ApolloClient,HttpLink,InMemoryCache,gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+})
+
+const query = gql`
+  query {
+    allPersons {
+      name,
+      phone,
+      address {
+        street,
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query( { query }).then(response => {
+    console.log(response.data)
+})
+
+
+
 const App = () => {
+
+
+
+
+
+
   const [page, setPage] = useState('authors')
 
   return (
