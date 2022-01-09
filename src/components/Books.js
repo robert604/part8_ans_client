20c30fdc13d21fed41998c9735883b7ed42e2d51
robert604 <robtemper@gmail.com>
2022-01-09 13:14:49 -0500

    submission for 8.9

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
+  if(result.loading) {
+    return <div>loading...</div>
+  }
 
-  const books = []
+  const books = result.data.allBooks
 
   return (
     <div>
